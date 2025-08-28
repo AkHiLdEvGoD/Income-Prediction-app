ccad0a7c76031cc62ba9342676d533cb9c3916a9
Akhil <akhdean@gmail.com>
2025-08-28 22:50:50 +0530

    test run 3

--- src/stages/stage_06_model_evaluation.py
+++ src/stages/stage_06_model_evaluation.py
@@ -6,26 +6,26 @@ from src.utils.logger import logger
 from src.utils.config import MODEL_PATH,TEST_DATA_PATH,ARTIFACTS_DIR
 import mlflow
 import dagshub
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
 def main():
-    load_dotenv()
-    dagshub_username = os.getenv('DAGSHUB_USERNAME')
-    dagshub_token = os.getenv('DAGSHUB_PASSWORD')
-    if not dagshub_token or not dagshub_username:
-        raise EnvironmentError('DAGSHUB_USERNAME and DAGSHUB_PASSWORD variable not set')
-    # tracking_uri = os.getenv('MLFLOW_TRACKING_URI')
-    # repo_name = os.getenv('DAGSHUB_REPO_NAME')
-    # repo_owner = os.getenv('DAGSHUB_REPO_OWNER')
-    os.environ['MLFLOW_TRACKING_USERNAME']= dagshub_username
-    os.environ['MLFLOW_TRACKING_PASSWORD'] = dagshub_token
-    dagshub_url = "https://dagshub.com"
+    # load_dotenv()
+    # dagshub_username = os.getenv('DAGSHUB_USERNAME')
+    # dagshub_token = os.getenv('DAGSHUB_PASSWORD')
+    # if not dagshub_token or not dagshub_username:
+    #     raise EnvironmentError('DAGSHUB_USERNAME and DAGSHUB_PASSWORD variable not set')
+    # # tracking_uri = os.getenv('MLFLOW_TRACKING_URI')
+    # # repo_name = os.getenv('DAGSHUB_REPO_NAME')
+    # # repo_owner = os.getenv('DAGSHUB_REPO_OWNER')
+    # os.environ['MLFLOW_TRACKING_USERNAME']= dagshub_username
+    # os.environ['MLFLOW_TRACKING_PASSWORD'] = dagshub_token
+    # dagshub_url = "https://dagshub.com"
     repo_owner = "AkHiLdEvGoD"
     repo_name = "Income-Prediction-app"
 
-    mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
+    # mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
     # mlflow.set_tracking_uri(tracking_uri)
-    # dagshub.init(repo_name=repo_name,repo_owner=repo_owner,mlflow=True)
+    dagshub.init(repo_name=repo_name,repo_owner=repo_owner,mlflow=True)
     mlflow.set_experiment('project_pipeline')
     with mlflow.start_run() as run:
         try:
